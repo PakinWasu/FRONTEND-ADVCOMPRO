1660e7af8d38c42dc89b99a12ac87e9224d27905
Pakin Wasu <pakin.wasu2002@gmail.com>
2024-09-11 10:54:32 +0700

    Ruk

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -3,7 +3,10 @@ const axios = require ('axios');
 const app = express();
 var bodyParser = require('body-parser');
 const path = require('path')
-const base_url = "http://localhost:3000";
+
+const base_url = "https://node65663-pakin-app.proen.app.ruk-com.cloud/";
+
+// const base_url = "http://localhost:3000";
 
 app.set('views',path.join(__dirname,"/public/views"))
 app.set('view engine', 'ejs');
